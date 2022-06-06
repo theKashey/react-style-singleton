49535770baf960bd9632ce713a58503ab118b8c1
Anton Korzunov <thekashey@gmail.com>
2022-06-06 20:38:32 +1000

    fix: correct singlenton behavior; disable dynamic style update

--- src/component.tsx
+++ src/component.tsx
@@ -2,13 +2,30 @@ import * as React from 'react';
 
 import { styleHookSingleton } from './hook';
 
-type Props = { styles: string };
+type Props = {
+  /**
+   * styles to apply
+   */
+  styles: string;
+  /**
+   * marks style as dynamic, so it will be reapplied on styles change
+   * note: this is not expected behavior from a "singleton"
+   * @default false
+   */
+  dynamic?: boolean;
+};
 
+/**
+ * create a Component to add styles on demand
+ * - styles are added when first instance is mounted
+ * - styles are removed when the last instance is unmounted
+ * - changing styles in runtime does nothing unless dynamic is set. But with multiple components that can lead to the undefined behavior
+ */
 export const styleSingleton = () => {
   const useStyle = styleHookSingleton();
 
-  const Sheet: React.FC<Props> = ({ styles }) => {
-    useStyle(styles);
+  const Sheet: React.FC<Props> = ({ styles, dynamic }) => {
+    useStyle(styles, dynamic);
 
     return null;
   };
--- src/hook.ts
+++ src/hook.ts
@@ -2,16 +2,39 @@ import * as React from 'react';
 
 import { stylesheetSingleton } from './singleton';
 
-export const styleHookSingleton = () => {
+/**
+ * creates a style on demand
+ */
+type StyleSingletonHook = (
+  /**
+   * styles to create
+   */
+  styles: string,
+  /**
+   * indication that styles should be reapplied on change
+   */
+  isDynamic?: boolean
+) => void;
+
+/**
+ * creates a hook to control style singleton
+ * @see {@link styleSingleton} for a safer component version
+ * @example
+ * ```tsx
+ * const useStyle = styleHookSingleton();
+ * ///
+ * useStyle('body { overflow: hidden}');
+ */
+export const styleHookSingleton = (): StyleSingletonHook => {
   const sheet = stylesheetSingleton();
 
-  return (styles: string) => {
+  return (styles, isDynamic) => {
     React.useEffect(() => {
       sheet.add(styles);
 
       return () => {
         sheet.remove();
       };
-    }, [styles]);
+    }, [styles && isDynamic]);
   };
 };
